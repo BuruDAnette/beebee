6b25fd2cf8ca0cd47a84a7e796cf434e9c658cf4
NãoÉaBruna <115668064+BuruDAnette@users.noreply.github.com>
2025-06-19 19:28:58 -0300

    fix: correção de erro

--- caronas/src/main/java/com/beebee/caronas/entities/Viagem.java
+++ caronas/src/main/java/com/beebee/caronas/entities/Viagem.java
@@ -2,18 +2,8 @@ package com.beebee.caronas.entities;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "tbViagem")
@@ -21,7 +11,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-
 public class Viagem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
